c5fbd7248dacaf88fd3a26746f910ca576077f02
Karim Smires <96136962+ks1686@users.noreply.github.com>
2024-06-25 15:07:08 -0400

    optimize imports

--- AwanChat.py
+++ AwanChat.py
@@ -1,12 +1,10 @@
-from operator import is_
-from awan_llm_api import AwanLLMClient, Role
-from awan_llm_api.completions import Completions, ChatCompletions
-from pysondb import db
 import os
 import subprocess
+
 import pvorca
-from pydub import AudioSegment
-from pydub.playback import play
+from awan_llm_api import AwanLLMClient, Role
+from awan_llm_api.completions import ChatCompletions
+from pysondb import db
 
 # API key and model name
 AWANLLM_API_KEY = "29d1a42a-37ad-49f2-bdc5-6309fdd89b4a"
@@ -36,10 +34,16 @@ menu_text = "\n".join(
 
 def speak(text):
     # Remove any invalid characters (e.g., *)
-    text = text.replace("*", "").replace("$", "USD: ")
+    text = (
+        text.replace("*", "")
+        .replace("$", "USD: ")
+        .replace("=", "equals")
+        .replace(" x ", " times ")
+        .replace(" + ", " plus ")
+    )
 
     # Synthesize speech using Orca and save to output.wav
-    alignments = orca.synthesize_to_file(text, "output.wav")
+    orca.synthesize_to_file(text, "output.wav")
 
     # Play the audio file using ffplay
     with open(os.devnull, "w") as devnull:
@@ -53,12 +57,41 @@ def speak(text):
     os.remove("output.wav")
 
 
+def payment():
+    print("Payment API called")
+    # extract the database items and quantity from the chat. items are formatted as below:
+    # * 2 Burgers: $8.99 x 2 = $17.98
+    # * 1 French Fries: $2.99
+    # we want: 2 Burgers, 1 French Fries
+
+
+# key phrase to confirm the order and end the conversation
+key_phrase = "Confirm my order"
+
+# Add a system message to the chat
 chat.add_message(Role.SYSTEM, f"Here is the food menu:\n{menu_text}")
+
+# add a system message to the chat for whenever key_phrase is mentioned, itemize the order
+chat.add_message(
+    Role.SYSTEM, "Please mention the item and the amount you would like to order"
+)
+
+# Initialize the content variable
+content = ""
 
 # Loop to take user input and generate responses
 while True:
     # Take user input
     user_input = input("User: ")
+
+    # if the user input is empty or exit, break the loop
+    if not user_input or user_input.lower() == "exit":
+        break
+
+    # if the user input is the key phrase, end the conversation
+    if key_phrase in user_input:
+        payment()
+        break
 
     # Add a user message to the chat
     chat.add_message(Role.USER, user_input)
