55ae02526bc9de33838fc79103b5991610a00acd
Karim Smires <96136962+ks1686@users.noreply.github.com>
2024-07-02 12:53:57 -0400

    Cleaned up files
    Names make more sense and things are far more straightforward.

--- app.py
+++ app.py
@@ -2,25 +2,17 @@ import json
 import os
 from secrets import token_urlsafe as generate_secret_key
 
+import awan_llm_api
+import awan_llm_api.completions
+import dotenv
 import ffmpeg
-from awan_llm_api import AwanLLMClient, Role
-from awan_llm_api.completions import ChatCompletions
-from dotenv import load_dotenv
-from flask import (
-    Flask,
-    jsonify,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    session,
-    send_from_directory,
-)
-from gtts import gTTS
+import flask
+import gtts
 
-from web_speech import speech_to_text
 
-app = Flask(__name__)
+from backend_scripts import speech_to_text
+
+app = flask.Flask(__name__)
 app.secret_key = generate_secret_key()
 app.static_folder = "static"
 app.config["UPLOAD_FOLDER"] = "static/audio"
@@ -29,7 +21,7 @@ if not os.path.exists(app.config["UPLOAD_FOLDER"]):
     os.makedirs(app.config["UPLOAD_FOLDER"])
 
 # Load environment variables
-load_dotenv()
+dotenv.load_dotenv()
 
 # Initialize recording process
 recording_process = None
@@ -43,10 +35,10 @@ if not AWANLLM_API_KEY or not MODEL_NAME:
     raise ValueError("Please provide the API key and model name")
 
 # Initialize the client
-client = AwanLLMClient(AWANLLM_API_KEY)
+client = awan_llm_api.AwanLLMClient(AWANLLM_API_KEY)
 
 # Initialize chat completions instance
-chat = ChatCompletions(MODEL_NAME)
+chat = awan_llm_api.completions.ChatCompletions(MODEL_NAME)
 
 # Load menu data from JSON
 with open("menu.json", "r") as f:
@@ -58,7 +50,7 @@ menu_text = "\n".join(
 )
 
 # Add a system message to the chat with the menu
-chat.add_message(Role.SYSTEM, f"Here is the menu:\n{menu_text}")
+chat.add_message(awan_llm_api.Role.SYSTEM, f"Here is the menu:\n{menu_text}")
 
 
 # Template filter to format price when displayed as currency
@@ -70,7 +62,7 @@ def format_currency(value):
 # Route for the main page with category buttons
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 
 # Route to handle category requests
@@ -81,7 +73,7 @@ def category_items(category_name):
         for item in menu_data
         if item["category"] == category_name and item["in_stock"]
     ]
-    return render_template(
+    return flask.render_template(
         "category_items.html", category_name=category_name, items=items_in_category
     )
 
@@ -89,9 +81,9 @@ def category_items(category_name):
 # Route to add item to the cart
 @app.route("/add_to_cart", methods=["POST"])
 def add_to_cart():
-    item_name = request.form.get("item_name")
-    item_price = request.form.get("item_price")
-    quantity_str = request.form.get("quantity")
+    item_name = flask.request.form.get("item_name")
+    item_price = flask.request.form.get("item_price")
+    quantity_str = flask.request.form.get("quantity")
 
     if quantity_str and quantity_str.isdigit():
         quantity = int(quantity_str)
@@ -99,12 +91,12 @@ def add_to_cart():
         return "Invalid quantity", 400
 
     # If the cart is not in the session, create it
-    if "cart" not in session:
-        session["cart"] = []
+    if "cart" not in flask.session:
+        flask.session["cart"] = []
 
     # Check if the item is already in the cart
     item_found = False
-    for item in session["cart"]:
+    for item in flask.session["cart"]:
         if item["name"] == item_name:
             item["quantity"] += quantity
             item_found = True
@@ -112,83 +104,85 @@ def add_to_cart():
 
     # If the item is not in the cart, add it
     if not item_found:
-        session["cart"].append(
+        flask.session["cart"].append(
             {"name": item_name, "price": item_price, "quantity": quantity}
         )
 
-    session.modified = True
-    return redirect(url_for("view_cart"))
+    flask.session.modified = True
+    return flask.redirect(flask.url_for("view_cart"))
 
 
 # Route to remove item from the cart
 @app.route("/remove_from_cart", methods=["POST"])
 def remove_from_cart():
-    item_name = request.form.get("item_name")
+    item_name = flask.request.form.get("item_name")
 
-    if "cart" in session:
-        session["cart"] = [
-            item for item in session["cart"] if item["name"] != item_name
+    if "cart" in flask.session:
+        flask.session["cart"] = [
+            item for item in flask.session["cart"] if item["name"] != item_name
         ]
-        session.modified = True
+        flask.session.modified = True
 
-    return redirect(url_for("view_cart"))
+    return flask.redirect(flask.url_for("view_cart"))
 
 
 # Route to update item quantity in the cart
 @app.route("/update_cart", methods=["POST"])
 def update_cart():
-    item_name = request.form.get("item_name")
-    new_quantity = request.form.get("quantity")
+    item_name = flask.request.form.get("item_name")
+    new_quantity = flask.request.form.get("quantity")
 
     if new_quantity and new_quantity.isdigit():
         new_quantity = int(new_quantity)
     else:
         return "Invalid quantity", 400
 
-    if "cart" in session:
-        for item in session["cart"]:
+    if "cart" in flask.session:
+        for item in flask.session["cart"]:
             if item["name"] == item_name:
                 item["quantity"] = new_quantity
                 break
 
-        session.modified = True
+        flask.session.modified = True
 
-    return redirect(url_for("view_cart"))
+    return flask.redirect(flask.url_for("view_cart"))
 
 
 # Route to view the cart
 @app.route("/cart")
 def view_cart():
-    cart = session.get("cart", [])
+    cart = flask.session.get("cart", [])
     total = sum(float(item["price"]) * item["quantity"] for item in cart)
-    return render_template("cart.html", cart=cart, total=total, hide_cart_button=True)
+    return flask.render_template(
+        "cart.html", cart=cart, total=total, hide_cart_button=True
+    )
 
 
 # Route to handle chat requests
 @app.route("/chat", methods=["POST"])
 def chat_api():
-    if request.is_json:
-        data = request.get_json()
+    if flask.request.is_json:
+        data = flask.request.get_json()
 
         # Add user message to chat
         user_message = data["message"]
 
-        chat.add_message(Role.USER, user_message)
+        chat.add_message(awan_llm_api.Role.USER, user_message)
         chat_response = client.chat_completion(chat)
 
         # Extract the content portion from the response
         content = chat_response["choices"][0]["message"]["content"]
-        return jsonify({"chat_response": content})
+        return flask.jsonify({"chat_response": content})
     else:  # Return error for non-JSON requests
         return "Invalid request", 400
 
 
 @app.route("/record", methods=["POST"])
 def record_audio():
-    if "audio" not in request.files:
-        return jsonify({"error": "No audio file received"}), 400
+    if "audio" not in flask.request.files:
+        return flask.jsonify({"error": "No audio file received"}), 400
 
-    audio_file = request.files["audio"]
+    audio_file = flask.request.files["audio"]
 
     try:
         audio_path_mp3 = os.path.join(app.config["UPLOAD_FOLDER"], "output.mp3")
@@ -207,29 +201,34 @@ def record_audio():
         if os.path.exists(audio_path_mp3):
             os.remove(audio_path_mp3)
 
-        return jsonify({"text": text}), 200
+        return flask.jsonify({"text": text}), 200
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return flask.jsonify({"error": str(e)}), 500
 
 
 @app.route("/tts", methods=["POST"])
 def stream_tts():
-    data = request.get_json()
+    data = flask.request.get_json()
     text = data["text"]
 
     # Generate speech
-    tts = gTTS(text)
+    tts = gtts.gTTS(text)
     audio_path = os.path.join(app.config["UPLOAD_FOLDER"], "output.mp3")
     tts.save(audio_path)
 
-    return jsonify({"audio_url": url_for("serve_audio", filename="output.mp3")}), 200
+    return (
+        flask.jsonify(
+            {"audio_url": flask.url_for("serve_audio", filename="output.mp3")}
+        ),
+        200,
+    )
 
 
 # Serve static files
 @app.route("/static/audio/<path:filename>")
 def serve_audio(filename):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+    return flask.send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 @app.route("/delete_audio", methods=["DELETE"])
@@ -238,11 +237,11 @@ def delete_audio():
         audio_path = os.path.join(app.config["UPLOAD_FOLDER"], "output.mp3")
         if os.path.exists(audio_path):
             os.remove(audio_path)
-            return jsonify({"message": "Audio file deleted"}), 200
+            return flask.jsonify({"message": "Audio file deleted"}), 200
         else:
-            return jsonify({"error": "Audio file not found"}), 404
+            return flask.jsonify({"error": "Audio file not found"}), 404
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return flask.jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
